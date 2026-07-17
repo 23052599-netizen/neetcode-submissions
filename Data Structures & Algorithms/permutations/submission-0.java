6756ddc31a2be45989891988c65b579f1f3dafad
snehal <23052599@kiit.ac.in>
2026-07-17 09:56:46 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>>ans = new ArrayList<>();
+        int n = nums.length;
+        
+        
+        recur(new ArrayList<>() , nums , ans ,new boolean[n]);
+        return ans;
+        
+    }
+    public void recur( List<Integer>prem , int[]nums , List<List<Integer>>ans , boolean[]pick ){
+        if(prem.size() == nums.length){
+            ans.add(new ArrayList<>(prem));
+            return;
+        }
+        for(int i = 0 ; i<nums.length ; i++){
+            if(!pick[i]){
+                prem.add(nums[i]);
+                pick[i] = true;
+                recur(prem , nums ,ans, pick);
+                prem.remove(prem.size()-1);
+                pick[i] = false;
+            }
+
+        }
+
+
+    }
+}
